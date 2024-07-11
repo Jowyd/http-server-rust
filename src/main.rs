90bcce9ff3d20537b77fbff3543db7469ca58878
Lyam <lyamkeovilay74@gmail.com>
2024-07-11 18:59:20 +0200

    concurency

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use itertools::Itertools;
 use std::error::Error;
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 #[allow(dead_code)]
 fn define_method(method: MethodType) {
@@ -68,6 +69,60 @@ impl Response {
     }
 }
 
+fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    println!("accepted new connection");
+    let mut buffer = [0; 1024];
+    let _ = stream.read(&mut buffer[..])?;
+    println!("received data:\n{}", String::from_utf8_lossy(&buffer[..]));
+
+    let request = String::from_utf8(buffer.into())?;
+    let lines = request.lines().collect_vec();
+    let start_line = lines.get(0).unwrap();
+    let split_start_line = start_line.split(' ').collect_vec();
+    let _method = split_start_line.get(0).unwrap().to_string();
+    let method_type: MethodType = _method.as_str().into();
+    let path = split_start_line.get(1).unwrap();
+    let _http_version = split_start_line.get(2).unwrap();
+
+    let host = lines
+        .iter()
+        .find(|line| line.starts_with("Host: "))
+        .unwrap()
+        .split(' ')
+        .collect_vec()
+        .get(1)
+        .unwrap();
+
+    if path == &"/" || path == &"/index.html" {
+        stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+    } else if path.starts_with("/echo/") {
+        let (_, data) = path.split_at(6);
+        let response = Response {
+            status_code: 200,
+            status_message: "OK".to_string(),
+            content_type: ContentType::Text,
+            body: data.to_string(),
+        };
+        stream.write(&response.to_bytes())?;
+    } else if path == &"/user-agent" {
+        let user_agent_line = lines
+            .iter()
+            .find(|line| line.starts_with("User-Agent: "))
+            .expect("cannot find user agent");
+        let (_, user_agent) = user_agent_line.split_at(12);
+        let response = Response {
+            status_code: 200,
+            status_message: "OK".to_string(),
+            content_type: ContentType::Text,
+            body: user_agent.to_string(),
+        };
+        stream.write(&response.to_bytes())?;
+    } else {
+        stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -77,56 +132,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                let mut buffer = [0; 1024];
-                let _ = _stream.read(&mut buffer[..])?;
-                println!("received data:\n{}", String::from_utf8_lossy(&buffer[..]));
-
-                let request = String::from_utf8(buffer.into())?;
-                let lines = request.lines().collect_vec();
-                let start_line = lines.get(0).unwrap();
-                let split_start_line = start_line.split(' ').collect_vec();
-                let _method = split_start_line.get(0).unwrap().to_string();
-                let method_type: MethodType = _method.as_str().into();
-                let path = split_start_line.get(1).unwrap();
-                let _http_version = split_start_line.get(2).unwrap();
-
-                let host = lines
-                    .iter()
-                    .find(|line| line.starts_with("Host: "))
-                    .unwrap()
-                    .split(' ')
-                    .collect_vec()
-                    .get(1)
-                    .unwrap();
-
-                if path == &"/" || path == &"/index.html" {
-                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
-                } else if path.starts_with("/echo/") {
-                    let (_, data) = path.split_at(6);
-                    let response = Response {
-                        status_code: 200,
-                        status_message: "OK".to_string(),
-                        content_type: ContentType::Text,
-                        body: data.to_string(),
-                    };
-                    _stream.write(&response.to_bytes())?;
-                } else if path == &"/user-agent" {
-                    let user_agent_line = lines
-                        .iter()
-                        .find(|line| line.starts_with("User-Agent: "))
-                        .expect("cannot find user agent");
-                    let (_, user_agent) = user_agent_line.split_at(12);
-                    let response = Response {
-                        status_code: 200,
-                        status_message: "OK".to_string(),
-                        content_type: ContentType::Text,
-                        body: user_agent.to_string(),
-                    };
-                    _stream.write(&response.to_bytes())?;
-                } else {
-                    _stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
-                }
+                thread::spawn(move || {
+                    handle_client(_stream).unwrap();
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
