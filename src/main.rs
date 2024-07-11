39811b8baab7fa59f1fb7e2a15d09f77ef06020b
Lyam <lyamkeovilay74@gmail.com>
2024-07-11 18:00:52 +0200

    echo path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use itertools::Itertools;
+use std::error::Error;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -12,17 +14,24 @@ fn main() {
             Ok(mut _stream) => {
                 println!("accepted new connection");
                 let mut buffer = [0; 1024];
-                let _ = _stream.read(&mut buffer[..]).expect("read");
-                let request = String::from_utf8_lossy(&buffer[..]);
-                println!("request: {}", request);
-                if request.contains("GET /index.html HTTP/1.1")
-                    || request.contains("GET / HTTP/1.1")
-                {
-                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                let _ = _stream.read(&mut buffer[..])?;
+                println!("received data: {}", String::from_utf8_lossy(&buffer[..]));
+
+                let request = String::from_utf8(buffer.into())?;
+                let lines = request.lines().collect_vec();
+                let start_line = lines.get(0).unwrap();
+                let split_start_line = start_line.split(' ').collect_vec();
+                let _method = split_start_line.get(0).unwrap();
+                let path = split_start_line.get(1).unwrap();
+                let _http_version = split_start_line.get(2).unwrap();
+
+                if path == &"/" || path == &"/index.html" {
+                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                } else if path.starts_with("/echo/") {
+                    let (_, data) = path.split_at(6);
+                    _stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", data.len(), data).as_bytes())?;
                 } else {
-                    _stream
-                        .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                        .expect("404 \n");
+                    _stream.write(b"HTTP/1.1 404 NOTFOUND\r\n\r\n")?;
                 }
             }
             Err(e) => {
@@ -30,4 +39,5 @@ fn main() {
             }
         }
     }
+    Ok(())
 }
