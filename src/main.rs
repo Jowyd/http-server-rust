205ace3a278690bcaa1aa3e4d75e479642a26447
Lyam <lyamkeovilay74@gmail.com>
2024-07-11 17:16:57 +0200

    404

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -11,7 +11,19 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                let mut buffer = [0; 1024];
+                let _ = _stream.read(&mut buffer[..]).expect("read");
+                let request = String::from_utf8_lossy(&buffer[..]);
+                println!("request: {}", request);
+                if request.contains("GET /index.html HTTP/1.1")
+                    || request.contains("GET / HTTP/1.1")
+                {
+                    _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                } else {
+                    _stream
+                        .write(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                        .expect("404 \n");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
